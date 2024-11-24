f1ab858e13b6081998e16643cef27d42f5e1be04
riomulya <riomulya75@gmail.com>
2024-11-24 18:50:45 +0700

    update

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
+const cors = require('cors');
+
+const TransactionsRoutes = require('./routes/transactions.routes');
+
+// const verifyToken = require('./middlewares/auth.middlewares');
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(cors({ origin: 'http://localhost:3000' })); // Izinkan permintaan dari localhost:3000
+
+// Gunakan middleware bodyParser untuk mengurai body permintaan
+app.use(bodyParser.json());
+
+app.use(express.json());
+app.use(cookieParser());
+
+// Atau jika ingin mengurai body berdasarkan URL-encoded data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(TransactionsRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
