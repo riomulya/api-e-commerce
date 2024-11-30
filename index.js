1ef0d09b1745b89fcf28918fc434c95153485757
Rio Mulya Syawal <riomulya75@gmail.com>
2024-11-30 11:32:06 +0700

    update

--- index.js
+++ index.js
@@ -1,27 +1,28 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+require('dotenv').config();
 const cors = require('cors');
-const bodyParser = require('body-parser');
-const TransactionsRoutes = require('../routes/transactions.routes');
+
+const TransactionsRoutes = require('./routes/transactions.routes');
+
+// const verifyToken = require('./middlewares/auth.middlewares');
 
 const app = express();
+const bodyParser = require('body-parser');
 
-// Middleware
-app.use(cors()); // Izinkan permintaan dari frontend
+app.use(cors({ origin: 'https://e-commerce-neon-nine.vercel.app/' })); // Izinkan permintaan dari localhost:3000
+
+// Gunakan middleware bodyParser untuk mengurai body permintaan
 app.use(bodyParser.json());
-app.use(cookieParser());
+
 app.use(express.json());
+app.use(cookieParser());
+
+// Atau jika ingin mengurai body berdasarkan URL-encoded data
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Rute API
 app.use(TransactionsRoutes);
 
-// Default route
 app.get('/', (req, res) => {
-  res.send('Hello World from Express on Vercel!');
+  res.send('Hello World');
 });
-
-// Hapus app.listen() dan ekspor aplikasi Express sebagai fungsi
-module.exports = (req, res) => {
-  app(req, res); // Menangani request dan response dengan Express
-};
